e7f6d989c9c0e2027b20dee88ecc854d4aede3b5
luismc <luismc@hackworld.org>
2021-08-03 12:47:12 +0100

    Update keymaps visualsations with keymapviz

--- keyboards/sofle/keymaps/luismcv/rules.mk
+++ keyboards/sofle/keymaps/luismcv/rules.mk
@@ -1,3 +1,7 @@
+# Update keymaps visualisations, using fortitude60's layout for now, which is similar enoughgit 
+.PHONY: %
+%:
+	keymapviz -r -c keyboards/sofle/keymaps/luismcv/keymapviz.toml -k fortitude60 keyboards/sofle/keymaps/luismcv/keymap.c > /dev/null
 
 OLED_DRIVER_ENABLE = yes
 ENCODER_ENABLE = yes
