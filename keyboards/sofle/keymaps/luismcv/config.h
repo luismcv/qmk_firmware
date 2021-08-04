58b5db951965814b7d61aa81708d304e58573e29
luismc <luismc@hackworld.org>
2021-08-04 11:27:36 +0100

    Home row mods

--- keyboards/sofle/keymaps/luismcv/config.h
+++ keyboards/sofle/keymaps/luismcv/config.h
@@ -6,3 +6,9 @@
 #endif
 
 //#define ONESHOT_TIMEOUT 5000
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Prevent accidental repeats of the tap-hold keys when typing quickly.
+#define TAPPING_FORCE_HOLD_PER_KEY
