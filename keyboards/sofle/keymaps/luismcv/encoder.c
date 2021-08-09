998fbaecec32e0f2429ccb835fb70053bdb0a18f
luismc <luismc@hackworld.org>
2021-08-09 13:54:30 +0100

    Changing encoders functions

--- keyboards/sofle/keymaps/luismcv/encoder.c
+++ keyboards/sofle/keymaps/luismcv/encoder.c
@@ -1,25 +1,18 @@
 #include QMK_KEYBOARD_H
 #include "keymap.h"
 
+void up_down_arrows(bool clockwise) {
+    if (clockwise) {
+        for (int i = 0; i < 5; i++) tap_code(KC_UP);
+    } else {
+        for (int i = 0; i < 5; i++) tap_code(KC_DOWN);
+    }
+}
+
 bool encoder_update_user(uint8_t index, bool clockwise) {
     switch (get_highest_layer(layer_state)) {
         case _COLEMAK:
         case _QWERTY:
-            if (index == 0) {
-                if (clockwise) {
-                    tap_code(KC_MS_WH_RIGHT);
-                } else {
-                    tap_code(KC_MS_WH_LEFT);
-                }
-            } else if (index == 1) {
-                if (clockwise) {
-                    tap_code(KC_MS_WH_UP);
-                } else {
-                    tap_code(KC_MS_WH_DOWN);
-                }
-            }
-            break;
-        case _NAV:
             if (index == 0) {
                 if (clockwise) {
                     tap_code(KC_VOLU);
@@ -27,14 +20,34 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
                     tap_code(KC_VOLD);
                 }
             } else if (index == 1) {
+                up_down_arrows(clockwise);
+            }
+            break;
+        case _NAV:
+            if (index == 0) {
                 if (clockwise) {
-                    tap_code(KC_PGDOWN);
+                    tap_code16(KC_MEDIA_NEXT_TRACK);
                 } else {
-                    tap_code(KC_PGUP);
+                    tap_code16(KC_MEDIA_PREV_TRACK);
                 }
+            } else if (index == 1) {
+                up_down_arrows(clockwise);
             }
             break;
         case _SYM:
+            if (index == 0) {
+                if (clockwise) {
+                    tap_code16(C(KC_Y));
+                } else {
+                    tap_code16(C(KC_Z));
+                }
+            } else if (index == 1) {
+                if (clockwise) {
+                    tap_code16(C(KC_RGHT));
+                } else {
+                    tap_code16(C(KC_LEFT));
+                }
+            }
             break;
         case _ADJUST:
             break;
